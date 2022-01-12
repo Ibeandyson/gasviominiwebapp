76e8f51cb337f63375b337b673619a363d0ca5f2
Ibeandyson <ibeandyson123@gmail.com>
2022-01-12 15:29:02 +0100

    FIxed folder naming

--- pages/api/purchaseCount.ts
+++ pages/api/purchaseCount.ts
@@ -1,11 +1,11 @@
-import {purchaseCount} from "../../src/backend/controller/staff"
+import {salesCount} from "../../src/backend/controller/staff"
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     switch (req.method) {
 
         case "GET": {
-            return purchaseCount(req, res);
+            return salesCount(req, res);
         }
 
     }
