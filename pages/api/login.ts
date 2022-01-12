b2a6caec84735d87e8e3cafccb5b81157156e69c
Ibeandyson <ibeandyson123@gmail.com>
2022-01-12 10:11:38 +0100

    fix 2

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,5 +1,5 @@
 import login from "../../src/backend/controller/auth/login"
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse} from 'next' 
 
 export default async function handler(req:  NextApiRequest, res:  NextApiResponse) {
     switch (req.method) {
